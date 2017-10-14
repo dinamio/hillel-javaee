6dc5f489b7d3f39fd30f535e668f852b8000eefb
Kostiuk_N <kost.nik88@gmail.com>
2017-10-14 18:34:02 +0300

    add fizzBuzz project to repo

---
+++ lab1/kostiuk/fizzBuzz/src/main/java/com/hillel/kostiuk/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,36 @@
+package com.hillel.kostiuk.fizzbuzz;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class FizzBuzzTest {
+
+    private static Map<Integer, String> participantHolder;
+
+    public static void main(String[] args) throws IOException {
+        participantHolder = new HashMap<>();
+        participantHolder.put(3, "Fizz");
+        participantHolder.put(5, "Buzz");
+        printValues(0, 100);
+    }
+
+    private static void printValues(int from, int to) {
+        IntStream.rangeClosed(from, to)
+                .mapToObj(FizzBuzzTest::getComputationString)
+                .filter(str -> !str.isEmpty())
+                .forEach(System.out::println);
+    }
+
+    private static String getComputationString(int i) {
+        StringBuilder result = new StringBuilder();
+        participantHolder.keySet().stream()
+                .filter(key -> i % key == 0)
+                .map(participantHolder::get)
+                .forEach(result::append);
+        return (result.length() == 0) || i == 0 ? "" : i + ": " + result;
+    }
+
+}
